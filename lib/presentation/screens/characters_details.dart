1605890b3daae3c67778e629e470d17c9a25afe2
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-06 10:50:11 +0200

    add animated text to details screen

--- lib/presentation/screens/characters_details.dart
+++ lib/presentation/screens/characters_details.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie/bussnies_logic/cubit/movie_cubit.dart';
+import 'package:movie/bussnies_logic/cubit/movie_state.dart';
 import 'package:movie/data/models/characters.dart';
 
 import '../widgets/details_screen_widget.dart';
@@ -24,9 +30,50 @@ class CharacterDetailsScreen extends StatelessWidget {
      ),
    );
  }
+Widget checkIfQuotesAreLoaded(CharactersState state){
+   if(state is QuotesLoaded){
+     return displayRandomQuoteOrEmptySpace(state);
+   }
+   else{
+     return const Center(child: CircularProgressIndicator(color: Colors.tealAccent,),);
+   }
+}
+  Widget displayRandomQuoteOrEmptySpace(state){
+   var quotes=(state).quotes;
+   if(quotes.lenght > 0){
+     int randomQuoteIndex=Random().nextInt(quotes.lenght-1);
+     return  SizedBox(
+       width: 250.0,
+       child: DefaultTextStyle(
+         textAlign: TextAlign.center,
+         style: const TextStyle(
+           fontSize: 20.0,
+           color: Colors.tealAccent,
+           shadows: [
+             Shadow(
+               blurRadius: 7,
+               color: Colors.tealAccent,
+               offset: Offset(0, 0),
+             )
+           ],
+         ),
+         child: AnimatedTextKit(
 
+           animatedTexts: [
+             TyperAnimatedText(quotes[randomQuoteIndex].qoute),
+           ],
+         ),
+       ),
+     );
+   }
+   else{
+     return Container();
+   }
+
+  }
   @override
   Widget build(BuildContext context) {
+   BlocProvider.of<CharactersCubit>(context).getCharactersQuotes(character.name);
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade800,
       body: CustomScrollView(
@@ -62,8 +109,11 @@ class CharacterDetailsScreen extends StatelessWidget {
                     const SizedBox(
                       height: 20,
                     ),
-
-
+                    BlocBuilder<CharactersCubit,CharactersState>(
+                        builder: (context,state){
+                          return checkIfQuotesAreLoaded(state);
+                        },
+                    ),
                   ],
                 ),
               ),
